19ced53ea150e2906d6d1e559930462989b59441
Muhammad Asad Qureshi <qureshiasad1000@gmail.com>
2020-02-28 23:04:49 +0500

    work added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl:"http://localhost:8080/"
-  //baseUrl:"https://thegigster.ca/gigster/"
+  // baseUrl:"http://localhost:8080/"
+  baseUrl:"http://thegigster.ca/gigster/"
 };
 
 /*
